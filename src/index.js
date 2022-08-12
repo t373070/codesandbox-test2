fe973c1f99d0fe222d593e722a1bb10bf33bb5ae
t373070 <59283837+t373070@users.noreply.github.com>
2022-08-12 15:57:26 +0900

    Pタグを追加

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "./styles.css";
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
+  <p> コミットテスト</p>
   We use the same configuration as Parcel to bundle this sandbox, you can find more
   info about Parcel 
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
